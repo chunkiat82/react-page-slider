f50d3ab5d6e53d08ec73540fd54b9b00e35f3d95
Raymond Ho <chunkiat82@gmail.com>
2015-11-10 23:41:21 +0800

    space issues

--- src/react-page-slider.jsx
+++ src/react-page-slider.jsx
@@ -3,30 +3,30 @@ import React from 'react';
 export default class PageSlider extends React.Component {
 
     render() {
-    	const overlayStyle = {
-        	position: 'fixed',
-        	top: 0,
-        	left: 0,
-        	background: 'rgba(0, 0, 0, 0.6)',
-        	zIndex: 5,
-        	width: '100%',
-        	height: '100%',
-        	visibility: 'hidden'
-    	}
+        const overlayStyle = {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            background: 'rgba(0, 0, 0, 0.6)',
+            zIndex: 5,
+            width: '100%',
+            height: '100%',
+            visibility: 'hidden'
+        }
 
-    	const topcornerStyle = {
-   			position:'absolute',
-   			top:10,
-   			right:10
-  		}
+        const topcornerStyle = {
+            position:'absolute',
+            top:10,
+            right:10
+        }
 
-    	const classes = this.props.show ? 'slideUp' : ''
+        const classes = this.props.show ? 'slideUp' : ''
 
-    	return (
-    		<div style={this.props.overlayStyle || overlayStyle} className={classes}>
-    			<a href="#" style={topcornerStyle} onClick={this.props.close}>Close</a>
-    			{this.props.children}
-    		</div>
-    	);
+        return (
+            <div style={this.props.overlayStyle || overlayStyle} className={classes}>
+                <a href="#" style={topcornerStyle} onClick={this.props.close}>Close</a>
+                {this.props.children}
+            </div>
+        );
     }
 }
