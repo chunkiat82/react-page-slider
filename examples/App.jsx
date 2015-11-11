27a71fe7d6bf0bc29d7b480dbd7699f3fa9abfac
Raymond Ho <chunkiat82@gmail.com>
2015-11-11 14:46:51 +0800

    fixing startup problem

--- examples/App.jsx
+++ examples/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import TestSlider from './TestSlider.jsx';
 
-export default class App extends React.Component {
+class App extends React.Component {
+
   render() {
     return (<TestSlider/>);
   };
 }
+
+export default App;
