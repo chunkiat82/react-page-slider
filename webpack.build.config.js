15665d36f683650aa07fcee4ef99644685ebb916
Raymond Ho <chunkiat82@gmail.com>
2016-11-24 13:00:11 +0800

    using babel 6 and webpack

---
+++ webpack.build.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./modules/index.js",
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: "index.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel'
+        }]
+    }
+};
